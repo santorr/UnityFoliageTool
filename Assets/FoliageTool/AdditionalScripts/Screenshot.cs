b2061d7712e87bdcf1a19f2b533992a8837e4a78
jeremy <duchesne.pro@gmail.com>
2023-04-20 19:59:34 +0200

    Add overwrite to screen capture

--- Assets/FoliageTool/AdditionalScripts/Screenshot.cs
+++ Assets/FoliageTool/AdditionalScripts/Screenshot.cs
@@ -7,9 +7,9 @@ public class Screenshot : MonoBehaviour
     public KeyCode firstInput = KeyCode.LeftControl;
     public KeyCode secondInput = KeyCode.S;
     [Header("Settings")]
+    public bool Overwrite = false;
     [Range(1, 5)] public int SizeMultpilier = 1;
-
-    private string _screenshotName = "Screenshot";
+    public string _screenshotName = "Screenshot";
 
     void Update()
     {
@@ -24,7 +24,11 @@ public class Screenshot : MonoBehaviour
 
     private void SaveScreenshot()
     {
-        string path = AssetDatabase.GenerateUniqueAssetPath(Application.dataPath + "/FoliageTool/Screenshots/" + _screenshotName + ".jpg");
+        string path = Application.dataPath + "/FoliageTool/Screenshots/" + _screenshotName + ".jpg";
+        if (!Overwrite)
+        {
+            path = AssetDatabase.GenerateUniqueAssetPath(path);
+        }
         ScreenCapture.CaptureScreenshot(filename: path, superSize: SizeMultpilier);
         UnityEditor.AssetDatabase.Refresh();
         Debug.Log("Saved screenshot.");
